efbf6c5f17e04773dced801eb4f1904e1ded2a1b
Andrew Plotkin <zarf@ZarfLent.local>
2025-01-18 23:26:59 -0500

    Jinja-style.

---
+++ build.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import os, os.path
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+destdir = 'htdocs'
+
+jenv = Environment(
+    loader = FileSystemLoader('templates'),
+    extensions = [
+    ],
+    autoescape = select_autoescape(),
+    keep_trailing_newline = True,
+)
+
+template = jenv.get_template('front.html')
+
+fl = open(os.path.join(destdir, 'index.html'), 'w')
+fl.write(template.render())
+fl.close()
